4b93aebe457ec5e13c3af6234ca34060226bea4c
siimon13 <chen.simon89@gmail.com>
2016-07-12 15:03:57 -0400

    Updated code

--- testing/sheets/views.py
+++ testing/sheets/views.py
@@ -3,15 +3,15 @@ from django.http import HttpResponse, HttpResponseNotFound
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from django.template import loader
 from django.core.urlresolvers import reverse
-import quickstart
+import codes
 import os.path
 
 # Create your views here.
 def index(request):
     #data = quickstart.display_Sheets()
-    data = quickstart.display_Sheets()
+    data = codes.display_Sheets()
     if data == "No data found." :
         return HttpResponseNotFound('<h1>Data Not Found</h1>')
     else:
         #return HttpResponse(data)
-        return render(request, os.path.realpath('.') + '/sheets/index.html', {'data' : data})
+        return render(request, 'index.html', {'data' : data})
